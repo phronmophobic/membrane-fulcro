f9205d9a4e816e017aeb784cfd92c31443535e4b
Adrian Smith <smith.adriane@gmail.com>
2020-12-26 20:46:49 -0800

    cleaning code a bit.

--- src/com/phronemophobic/todo.clj
+++ src/com/phronemophobic/todo.clj
@@ -47,38 +47,18 @@
       app-root)))
 
 
-
-;; (def app-view (atom nil))
-
-;; (skia/run #(deref app-view))
-
 (defn component->view [component]
   (let [opts (comp/component-options component)
         render (:render opts)]
     (render component)))
 
 
+(defmutation request-focus
+  "docstring"
+  [{:keys [focus-id]}]
 
-
-(defn ui-zip [ui]
-  (z/zipper #(and (seq (membrane.ui/children %))
-                  (satisfies? membrane.ui/IMakeNode %))
-            membrane.ui/children
-            membrane.ui/make-node
-            ui))
-
-(defn render-all [ui]
-  (loop [loc (ui-zip ui)]
-    (if (z/end? loc)
-      (z/root loc)
-      (recur (let [loc (z/edit loc
-                               (fn [x]
-                                 (loop [x x]
-                                   (if (comp/component-instance? x)
-                                     (recur (component->view x))
-                                     x))))]
-               (z/next loc))))))
-
+  (action [{:keys [state] :as env}]
+          (swap! state assoc :focus focus-id)))
 
 (defn dispatch! [app tx]
   (when (seq tx)
@@ -176,22 +156,9 @@
          (when-let [cdm (-> app ::app/config :client-did-mount)]
            (cdm app)))))))
 
-;; (my-mount! app Root :my-root)
-
-;; let [[k v] (comp/ident comp initial-state)]
-
-#_(defsc Root [this {::keys [child child-type]}]
-  ;; {:query [::child ::child-type]}
-  (child-type child))
-
-#_(defsc Root [this {::keys [child]}]
-  {:query [{::child (comp/get-query Child)}]
-   :initial-state {}}
-  (child-factory child))
-
-
-#_(def root-factory (comp/factory Root))
-
+#_(defsc Root [this props]
+  {:query [{::child (comp/get-query Child)}]}
+  (component->view (component->view (child-factory child))))
 
 (defn make-root [Child initial-state]
   (clojure.core/let
@@ -227,9 +194,7 @@
   [{:keys [child-ident]}]
   (action [{:keys [state] :as env}]
           (swap! state assoc ::child child-ident)
-          nil
-          #_(swap! state ...))
-)
+          nil))
 
 (defn quick-view!
   "Pop up a window of the component with the state"
@@ -319,24 +284,6 @@
        app))))
 
 
-(defmutation toggle-checkbox
-  "docstring"
-  [{:checkbox/keys [id]}]
-  (action [{:keys [state] :as env}]
-          ;; (prn (get-in @state[:checkbox/id :checkbox/checked?]) )
-          (swap! state update-in [:checkbox/id id  :checkbox/checked?] not)
-          nil)
-  )
-(def dropdown-table :bootstrap.dropdown/by-id)
-(defn dropdown-ident [id-or-props]
-  [dropdown-table (::id id-or-props)]
-  #_(if (map? id-or-props)
-      [(:type id-or-props) (::id id-or-props)]
-      [dropdown-table id-or-props]))
-
-
-
-
 (defn wrap-mouse-move-global [handler body]
   (ui/on-mouse-move-global
    (fn [pos]
@@ -353,8 +300,7 @@
                          (if (vector? intent)
                            (list `membrane-dispatch! {:intent intent})
                            intent))
-                       intents))
-                    ))]
+                       intents))))]
     (wrap-mouse-move-global wrapper
      (ui/wrap-on :mouse-down wrapper
                  :key-press wrapper
@@ -377,12 +323,7 @@
           nil
           #_(swap! state ...)))
 
-(defmutation request-focus
-  "docstring"
-  [{:keys [focus-id]}]
 
-  (action [{:keys [state] :as env}]
-          (swap! state assoc :focus focus-id)))
 
 
 
@@ -477,119 +418,17 @@
 
 (fulcroize-membrane-component basic/textarea-view "textarea" TextArea)
 
-(defsc TextArea [ this {:textarea/keys [cursor
-                                        text
-                                        down-pos
-                                        mpos
-                                        select-cursor
-                                        last-click
-                                        font
-                                        id
-                                        border?]
-                        :keys [focus]}]
-  {:initial-state (fn [props]
-                    (merge {:textarea/cursor 0
-                            :textarea/text ""
-                            :textarea/border? true
-                            :textarea/id (uuid)}
-                           props))
-   :ident :textarea/id
-   :query [[:focus '_]
-           :textarea/cursor
-           :textarea/text
-           :textarea/down-pos
-           :textarea/mpos
-           :textarea/select-cursor
-           :textarea/last-click
-           :textarea/font
-           :textarea/border?
-           :textarea/id]}
-  (do
-    ;; (prn (list "=" focus [:textarea/id id] (= focus [:textarea/id id])) )
-    (my-events
-     (ui/on
-      ::basic/request-focus
-      (fn []
-        [`(request-focus {:focus-id ~[:textarea/id id]})])
-      (basic/textarea-view
-       :cursor cursor
-       :$cursor [:textarea/id (list 'keypath id) :textarea/cursor]
-       :focus? (= focus [:textarea/id id])
-       :text text
-       :$text [:textarea/id (list 'keypath id) :textarea/text]
-       :down-pos down-pos
-       :$down-pos [:textarea/id (list 'keypath id) :textarea/down-pos]
-       :mpos mpos
-       :$mpos [:textarea/id (list 'keypath id) :textarea/mpos]
-       :select-cursor select-cursor
-       :$select-cursor [:textarea/id (list 'keypath id) :textarea/select-cursor]
-       :last-click last-click
-       :$last-click [:textarea/id (list 'keypath id) :textarea/last-click]
-       :font font
-       :border? border?)))))
 
-(def ui-textarea* (comp/factory TextArea))
-(defn ui-textarea [props]
-  (component->view (ui-textarea* props)))
-
-(comment
-  (quick-view! TextArea {:textarea/cursor 0
-                         :textarea/text "hello"
-                         :textarea/border? true
-                         :textarea/id 1})
-  ,)
-
-(defsc TextAreaRoot [this {:keys [textarea1 textarea2 ]}]
-  {:initial-state (fn [_]
-                    {:textarea1 (comp/get-initial-state TextArea
-                                                        {:textarea/text "one"
-                                                         :textarea/id (uuid)})
-                     :textarea2 (comp/get-initial-state TextArea
-                                                        {:textarea/text "two"
-                                                         :textarea/id (uuid)})})
-   :query [{:textarea1 (comp/get-query TextArea)}
-           {:textarea2 (comp/get-query TextArea)}]}
-  (ui/vertical-layout
-   (ui/label "Text Area App")
-   (ui-textarea textarea1)
-   (ui-textarea textarea2)))
-
-(def ui-textarearoot (comp/factory TextAreaRoot))
-
-(comment
-  (quick-view-root! TextAreaRoot {}))
-
-(defsc Checkbox [this {:checkbox/keys [id checked?] :as props}]
-  {:ident :checkbox/id
-   :query [:checkbox/id :checkbox/checked?]
-   :initial-state (fn [params]
-                    (merge
-                     {:checked? false
-                      :id (uuid)}
-                     params))
-   :use-hooks? true}
-
-  (ui/on
-   :mouse-down
-   (fn [_]
-     [(toggle-checkbox {:checkbox/id id})])
-   (ui/checkbox checked?)))
-
-
-(def checkbox-factory (comp/factory Checkbox))
-(defn ui-checkbox [props]
-  (component->view (checkbox-factory props)))
+(fulcroize-membrane-component basic/checkbox "checkbox" Checkbox)
 
 
 (defmutation toggle-todo
   "docstring"
   [{:todo/keys [id] :as arg}]
   (action [{:keys [state] :as env}]
-          ;; (prn (get-in @state  [:todo/id]) )
           (swap! state update-in [:todo/id id :todo/checked?] not)
 
-          nil)
-  )
+          nil))
 
 (defmutation update-todo-description
   ""
@@ -605,7 +444,7 @@
           nil))
 
 (defsc TodoItem [this {:todo/keys [id checked? description ]
-                       :keys [ta cb]
+                       :keys [todo-textarea todo-checkbox]
                        :as props}]
   {:initial-state (fn [params]
                     (let [tid (get params :todo/id (uuid))]
@@ -613,32 +452,30 @@
                       {:todo/checked? false
                        :todo/id tid
                        :todo/description ""
-                       :ta (comp/get-initial-state TextArea {:textarea/id [::todo-item tid]})
-                       :cb (comp/get-initial-state Checkbox {:checkbox/id [::todo-item tid]})}
+                       :todo-textarea (comp/get-initial-state TextArea {:textarea/id [::todo-item tid]
+                                                             :textarea/text (get params :todo/description "")})
+                       :todo-checkbox (comp/get-initial-state Checkbox {:checkbox/id [::todo-item tid]})}
                       params)))
-   :use-hooks? true
    :ident :todo/id
    :query [:todo/id
            :todo/checked?
            :todo/description
-           {:cb (comp/get-query Checkbox)}
-           {:ta (comp/get-query TextArea)}]}
+           {:todo-checkbox (comp/get-query Checkbox)}
+           {:todo-textarea (comp/get-query TextArea)}]}
   (ui/horizontal-layout
-   (ui/on
-    `toggle-checkbox
-    (fn [checkbox-id]
-      [ ;; (toggle-todo nil)
-       (toggle-checkbox checkbox-id)
-       (toggle-todo {:todo/id id})])
-    (ui-checkbox cb))
    (ui/on-bubble
     (fn [intents]
-
+      (when (seq intents)
+        (concat intents
+                [(toggle-todo {:todo/id id})])))
+    (ui-checkbox todo-checkbox))
+   (ui/on-bubble
+    (fn [intents]
       (when (seq intents)
         (concat intents
                 [(update-todo-description {:todo/id id
-                                           :textarea/id (:textarea/id ta)})])))
-    (ui-textarea ta))))
+                                           :textarea/id (:textarea/id todo-textarea)})])))
+    (ui-textarea todo-textarea))))
 
 
 
@@ -656,37 +493,56 @@
 
 (defmutation add-todo
   "docstring"
-  [{id :todo-list/id}]
+  [{id :todo-list/id
+    todo-description :todo/description}]
   (action [{:keys [app state] :as env}]
           (let [tid (uuid)]
             ;; (prn (get-in @state[:checkbox/id :checkbox/checked?]) )
-            (merge/merge-component! app TodoItem (comp/get-initial-state TodoItem {:todo/id tid}))
+            (merge/merge-component! app TodoItem (comp/get-initial-state TodoItem {:todo/id tid
+                                                                                   :todo/description todo-description}))
             (swap! state update-in [:todo-list/id id :todo-list/todo-list] conj [:todo/id tid]))
           nil))
 
-(defsc TodoList [this {:todo-list/keys [id todo-list button]}]
+
+(defmutation clear-textarea
+  "docstring"
+  [{id :textarea/id}]
+  (action [{:keys [state] :as env}]
+          (swap! state update-in [:textarea/id id]
+                 assoc
+                 :textarea/text ""
+                 :textarea/cursor 0)
+          nil))
+
+(defsc TodoList [this {:todo-list/keys [id todo-list button new-todo-textarea]}]
   { ;;:initial-state
    :ident :todo-list/id
    :initial-state (fn [params]
-                    (merge
-                     {:todo-list/id (uuid)
-                      :todo-list/todo-list []
-                      :todo-list/button (comp/get-initial-state Button
-                                                                {:button/text "Add Todo"})}
-                     params))
-   :use-hooks? true
+                    (let [tlid (get params :todo-list/id (uuid))]
+                      (merge
+                       {:todo-list/id tlid
+                        :todo-list/todo-list []
+                        :todo-list/button (comp/get-initial-state Button
+                                                                  {:button/text "Add Todo"})
+                        :todo-list/new-todo-textarea (comp/get-initial-state TextArea
+                                                                             {:textarea/id [:todo-list/id tlid]})}
+                       params)))
    :query [{:todo-list/todo-list (comp/get-query TodoItem)}
            {:todo-list/button (comp/get-query Button)}
+           {:todo-list/new-todo-textarea (comp/get-query TextArea)}
            :todo-list/id]}
   (apply
    ui/vertical-layout
-   (ui/on
-    :request-focus
-    (fn [])
-    :mouse-down
-          (fn [_]
-            [(add-todo {:todo-list/id id})])
-          (ui-button button))
+   (ui/horizontal-layout
+    (ui/on
+     :mouse-down
+     (fn [_]
+       [(add-todo {:todo-list/id id
+                   :todo/description (:textarea/text new-todo-textarea)})
+        (clear-textarea {:textarea/id (:textarea/id new-todo-textarea)})
+        ])
+     (ui-button button))
+    (ui-textarea new-todo-textarea))
    (map ui-todo-item todo-list)))
 
 (comment
@@ -698,41 +554,6 @@
     (quick-view! Checkbox {:checkbox/id 1
                            :checkbox/checked? true})))
 
-#_(quick-view! Todo
-             {:todo/id 1
-              :todo/description 10
-              :todo/complete? false}
-             
-             )
-
-
-(defsc CheckboxTest [this {:keys [checkbox1
-                                  checkbox2
-                                  checkbox3
-                                  foo] :as props}]
-  {:initial-state {:foo :bar}
-   :query [{:checkbox1 (comp/get-query Checkbox)}
-           {:checkbox2 (comp/get-query Checkbox)}
-           {:checkbox3 (comp/get-query Checkbox)}
-           :foo]
-   :use-hooks? true}
-  (do
-    (ui/vertical-layout
-     (my-checkbox (or checkbox1 {:checkbox/id 1}))
-     (my-checkbox (or checkbox2 {:checkbox/id 2}))
-     (my-checkbox (or checkbox3 {:checkbox/id 3})))))
-
-#_(quick-view-root! CheckboxTest {:checkbox1 [:checkbox/id 1]
-                                :checkbox2 [:checkbox/id 2]
-                                :checkbox3 [:checkbox/id 3]
-                                :checkbox/id {1 {:checkbox/id 1}
-                                              2 {:checkbox/id 2}
-                                              3 {:checkbox/id 3}}})
-
-
-#_(quick-view-root! CheckboxTest {:checkbox1 [:checkbox/id 1]
-                                :checkbox2 [:checkbox/id 2]
-                                :checkbox3 [:checkbox/id 3]})
 
 ;; cheat sheet
 (comment
